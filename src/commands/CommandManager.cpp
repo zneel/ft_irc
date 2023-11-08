ae9b2059c2474f3aefbd1e8dc9698c4df4b6ae32
mhoyer <mathieuhoyer@gmail.com>
2023-11-08 15:31:02 +0100

    RPL_ISUPPORT

--- src/commands/CommandManager.cpp
+++ src/commands/CommandManager.cpp
@@ -39,4 +39,42 @@ void CommandManager::doCommands(std::deque<Message> &msgs, User *sender)
             sender->send(RPL_WELCOME(sender->username, sender->nickmask));
         }
     }
+    if (sender->isupportTokenPackCount != 2)
+    {
+        if (sender->isupportTokenPackCount == 0)
+        {
+            std::string buf;
+            buf.append(AWAYLEN);
+            buf.append(CASEMAPPING);
+            buf.append(CHANLIMIT);
+            buf.append(CHANMODES);
+            buf.append(CHANNELLEN);
+            buf.append(CHANTYPES);
+            buf.append(ELIST);
+            buf.append(EXCEPTS);
+            buf.append(EXTBAN);
+            buf.append(HOSTLEN);
+            buf.append(INVEX);
+            buf.append(KICKLEN);
+            buf.append(MAXLIST);
+            buf.append(MAXTARGET);
+            buf.append(MODES);
+            append(sender->getSendBuffer(), RPL_ISUPPORT(sender->nick, buf));
+        }
+        else if (sender->isupportTokenPackCount == 1)
+        {
+            std::string buf;
+            buf.append(NETWORK);
+            buf.append(NICKLEN);
+            buf.append(PREFIX);
+            buf.append(SAFELIST);
+            buf.append(SILENCE);
+            buf.append(STATUSMSG);
+            buf.append(TARGMAX);
+            buf.append(TOPICLEN);
+            buf.append(USERLEN);
+            append(sender->getSendBuffer(), RPL_ISUPPORT(sender->nick, buf));
+        }
+        sender->isupportTokenPackCount++;
+    }
 }
